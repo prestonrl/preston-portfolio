92c285c86e1ed232cbee99be7ad31a77f19abc9a
tubasthebest <tubasthebest@gmail.com>
2021-04-07 21:37:32 -0600

    add photos and links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,23 +1,82 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/projects/miles-ahead.PNG"
+import miles from "../../assets/projects/miles-ahead.PNG"
+import iss from "../../assets/projects/ISS-proximity.PNG"
+import quiz from "../../assets/projects/coding-quiz.png"
+import note from "../../assets/projects/note-taker.PNG"
+import blog from "../../assets/projects/tech-blog.png"
+import weather from "../../assets/projects/weather-dashboard.png"
 
 function Portfolio(props) {
-    const currentCategory = {
-        name: "Miles Ahead",
-        description: "A fitness tracking app"
-    }
+    
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.description}</p>
+            <h2>Miles Ahead</h2>
+            <p>A fitness tracking app</p>
             <div className="flex-row">
-                <img
-                    src={photo}
+                <a href="https://miles-ahead.herokuapp.com/login" >
+                <img 
+                    src={miles}
                     alt="Miles Ahead App"
                     className="img-thumbnail mx-1"
                 />
+                </a>
             </div>
+            <h2>International Space Station Proximity</h2>
+            <p>Find out how far away the ISS is</p>
+            <div className="flex-row">
+                <a href="https://prestonrl.github.io/iss-location/">
+                <img
+                    src={iss}
+                    alt="ISS location"
+                    className="img-thumbnail mx-1"
+                />
+                </a>
+            </div>
+            <h2>Coding quiz</h2>
+            <p>Challenge yourself with this quick coding quiz</p>
+            <div className="flex-row">
+                <a href="https://prestonrl.github.io/coding-quiz/">
+                <img
+                    src={quiz}
+                    alt="coding quiz"
+                    className="img-thumbnail mx-1"
+                />
+                </a>
+            </div>
+            <h2>Note Taker</h2>
+            <p>A place to store thoughts</p>
+            <div className="flex-row">
+                <a href="https://note-taker-pl.herokuapp.com/">
+                <img
+                    src={note}
+                    alt="Note Taker"
+                    className="img-thumbnail mx-1"
+                />
+                </a>
+            </div>
+            <h2>The Tech Blog</h2>
+            <p>A blogging site for the tech enthusiast </p>
+            <div className="flex-row">
+                <a href="https://thawing-scrubland-77713.herokuapp.com/">
+                <img
+                    src={blog}
+                    alt="Tech Blog"
+                    className="img-thumbnail mx-1"
+                />
+                </a>
+            </div>
+            <h2>Weather Dashboard</h2>
+            <p>Check the weather around the world </p>
+            <div className="flex-row">
+                <a href="https://prestonrl.github.io/weather-dashboard/" >
+                <img
+                    src={weather}
+                    alt="Weather Dashboard"
+                    className="img-thumbnail mx-1"
+                />
+                </a>
+            </div>
+            
         </section>
     );
 }
