c212ce31655206f668832d1b0a140fa357736578
tubasthebest <tubasthebest@gmail.com>
2021-04-10 18:16:40 -0600

    SPA

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
         <section>
             <h1 id="about">Preston Larson</h1>
             <p>I am a new web developer located in Utah. I currently work as a Team Lead of Customer Service at a bank. I and am looking to put my newly aquired coding skills to use at a new company.</p>
-            <p>I am currently attending the Univiersity of Utah's Coding Bootcamp to become a Full Stack Web Developer.</p>
+            <p>I am currently attending the University of Utah's Coding Bootcamp to become a Full Stack Web Developer.</p>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
         </section>
     );
